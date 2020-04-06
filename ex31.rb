33beb1c55e2b5e52164b10b238025ad02b945141
chrph <cxpdev@outlook.com>
2020-04-06 13:41:55 +0200

    Made new parts of the game and added decisions that people can make.

--- ex31.rb
+++ ex31.rb
@@ -1,10 +1,14 @@
-puts "You enter a dark room with two doors. Do you go through door #1 or door #2?"
+puts %{
+You enter a dark room with two doors. Door #1 has deep, long scratches on it. 
+\rDoor #2 is so black, it looks like a deep hole into nowhere if it were not for the doorknob.
+Do you go through door #1 or door #2?
+}
 
 print "> "
-door = $stdin.gets.chomp
+door = $stdin.gets.chomp.downcase
 
 if door == "1"
-    puts "There's a giant bear here eating a cheese cake. What do you do?"
+    puts "There's a giant evil bear here eating a cheese cake. What do you do?"
     puts "1. Take the cake."
     puts "2. Scream at the bear."
 
@@ -12,7 +16,19 @@ if door == "1"
     bear = $stdin.gets.chomp
 
     if bear ==="1"
-        puts "The bear eats your face off. Good job!"
+        puts "The bear attacks you! Do you run, stand your ground, or attack?"
+        print "> "
+        action = $stdin.gets.chomp.downcase
+
+        if action == "run"
+            puts "The bear is way faster than you! It attacks you and rips your arms off!"
+        elsif action == "stand your ground"
+            puts "The bear thinks you're funny for thinking you can stand your ground. It laughs a dark, thunderous laugh and scratches your face!"
+        elsif action == "attack"
+            puts "The bear is way stronger than you and now it's agitated! It rips your head off and you die instantly."
+        else 
+            puts "%s doesn't work and the bear eats you!" % action
+        end
     elsif bear == "2"
         puts "The bear eats your legs off. Good job!"
     else 
