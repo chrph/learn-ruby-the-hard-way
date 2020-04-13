07e73486405b398e0464d7d4334d470a56909f0f
chrph <cxpdev@outlook.com>
2020-04-13 16:46:31 +0200

    Exercise 34 Accessing Elements of Arrays

---
+++ ex34.rb
@@ -0,0 +1,36 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts """The animal at 1.
+The animal at 1 is the 2nd animal and is a ruby. 
+Right answer: #{animals[1]}
+
+The third (3rd) animal.
+The 3rd animal is at 2 and is a peacock. The animal at 2 is the 3rd animal and is a peacock.
+Right answer: #{animals[2]}
+
+The first (1st) animal.
+The 1st animal is at 0 and is a bear. The animal at 0 is the 1st animal and is a bear. 
+Right answer: #{animals[0]}
+
+The animal at 3.
+The animal at 3 is the 4th animal and is a kangaroo. 
+Right answer: #{animals[3]}
+
+
+The fifth (5th) animal.
+The 5th animal is at 4 and is a whale.
+Right answer: #{animals[4]}
+
+The animal at 2.
+The animal at 2 is the 3rd animal and is a peacock.
+Right answer: #{animals[2]}
+
+The sixth (6th) animal.
+The 6th animal is at 5 and is a platypus. The animal is at 5 is the 6th animal and is a platypus.
+Right answer: #{animals[5]}
+
+The animal at 4.
+The animal at 4 is the 5th animal and is a whale.
+Right answer: #{animals[4]}
+
+"""
