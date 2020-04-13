ba326a6fdb9c83481c325c1fc571d4bfa6c82e28
chrph <cxpdev@outlook.com>
2020-04-13 15:58:49 +0200

    Converted while-loop to a method and replaced 6 in the test with a variable

--- ex33whileloops.rb
+++ ex33whileloops.rb
@@ -1,26 +1,33 @@
-i = 0
-# empty array to store stuff
-numbers = []
 
+
+def lessThanSix(i,n)
+    numbers = []
 # while i is less than 6, print this string to the console
 # than push i to the numbers array
-while i < 6
-    puts "At the top i is #{i}"
-    numbers.push(i)
-    # At the top is i 0
-    # [0]
+    while i < n
+        puts "At the top i is #{i}"
+        numbers.push(i)
+        # At the top is i 0
+        # [0]
+        puts "____________________"
 
-    # i + 1 
-    i += 1
-    puts "Numbers now: ", numbers
-    # Numbers now: 0
-    puts "At the bottom i is #{i}"
-    # At the bottom is 1
+        # i + 1 
+        i += 1
+        puts "Numbers now: ", numbers
+        # Numbers now: 0
+        puts "At the bottom i is #{i}"
+        # At the bottom is 1
+    end
+    puts "The numbers: "
+    numbers.each { |num| puts num }
 end
 
+i = 0
+n = 10
+lessThanSix(i,n)
 # when the while loop equals false and ends, print the numbers
-puts "The numbers: "
+
 
 # remember you can write this 2 other ways?
 # for each element in numbers array print the element on a new line
-numbers.each { |num| puts num }
+# numbers.each { |num| puts num }
