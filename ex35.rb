c6d8d40eb6e750434bb20806cadd3a1dfde4d89b
chrph <cxpdev@outlook.com>
2020-04-14 14:45:40 +0200

    Refactored code for method gold_room

--- ex35.rb
+++ ex35.rb
@@ -2,19 +2,16 @@ def gold_room
     puts "This room is full of gold. How much do you take?"
     
     print "> "
-    choice = $stdin.gets.chomp
-
-    #this line has a bug, so fix it
-    if choice.include?("0") || choice.include?("1")
-        how_much = choice.to_i
-    else
+    begin
+        choice = Integer(gets.chomp)
+    rescue ArgumentError, TypeError
         dead("Man, learn to type a number.")
     end
-
-    if how_much < 50
+    
+    if choice < 50
         puts "Nice, you're not greedy, you win!"
         exit(0)
-    else
+    else choice > 50
         dead("You greedy bastard!")
     end
 end
