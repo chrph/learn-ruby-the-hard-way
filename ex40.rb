b9b634180124c0aefe31f434b12b5bf8474195b9
chrph <cxpdev@outlook.com>
2020-04-22 14:13:52 +0200

    Hack the code to make it do different things than the original

--- ex40.rb
+++ ex40.rb
@@ -14,22 +14,17 @@ end
 
 # created a new Song object using the blueprint 
 # in the initialize method of the Song class
-happy_bday_lyrics = ["Happy birthday to you",
-    "I don't want to get sued",
-    "So I'll stop right there"]
-happy_bday = Song.new(happy_bday_lyrics)
 
+array = []
+print "Let's create new lyrics. Line 1: "
+your_lyrics = gets.chomp
 
-bulls_lyrics = ["They rally around tha family", 
-    "With pockets full of shellls"]
-bulls_on_parade = Song.new(bulls_lyrics)
+print "Line 2: "
+my_lyrics = gets.chomp
 
-ring_lyrics = ["Ring-a-ring-rosies", "A pocket full of posies",
-    "A tissue, a tissue", "We all fall down"]
-ring = Song.new (ring_lyrics)
+array.push(your_lyrics,my_lyrics)
+new_song = Song.new(array)
 
 # object.method.argument
 # call sing_me_a_song on happy_bday
-happy_bday.sing_me_a_song()
-bulls_on_parade.sing_me_a_song()
-ring.sing_me_a_song()
+new_song.sing_me_a_song()
