eb1ec8ff860dc6af735a064144a6329457c27b28
chrph <cxpdev@outlook.com>
2020-04-22 14:03:25 +0200

    Added a new song object and commented in English what the code is doing

--- ex40.rb
+++ ex40.rb
@@ -1,6 +1,9 @@
 class Song
 
+    # def function with one parameter
     def initialize(lyrics)
+        # @lyrics is an object scope variable
+        # the variable lyrics takes the argument lyrics as it's value
         @lyrics = lyrics
     end
 
@@ -9,6 +12,8 @@ class Song
     end
 end
 
+# created a new Song object using the blueprint 
+# in the initialize method of the Song class
 happy_bday = Song.new(["Happy birthday to you",
                 "I don't want to get sued",
                 "So I'll stop right there"])
@@ -16,5 +21,11 @@ happy_bday = Song.new(["Happy birthday to you",
 bulls_on_parade = Song.new(["They rally around tha family", 
                     "With pockets full of shellls"])
 
+ring = Song.new (["Ring-a-ring-rosies", "A pocket full of posies",
+                    "A tissue, a tissue", "We all fall down"])
+
+# object.method.argument
+# call sing_me_a_song on happy_bday
 happy_bday.sing_me_a_song()
 bulls_on_parade.sing_me_a_song()
+ring.sing_me_a_song()
