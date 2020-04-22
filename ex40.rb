17b9013511dec02ccdc77096879ae193e16d9e3c
chrph <cxpdev@outlook.com>
2020-04-22 14:08:08 +0200

    Put lyrics in a separate variable then pass that variable to the class to use instead.

--- ex40.rb
+++ ex40.rb
@@ -14,15 +14,19 @@ end
 
 # created a new Song object using the blueprint 
 # in the initialize method of the Song class
-happy_bday = Song.new(["Happy birthday to you",
-                "I don't want to get sued",
-                "So I'll stop right there"])
+happy_bday_lyrics = ["Happy birthday to you",
+    "I don't want to get sued",
+    "So I'll stop right there"]
+happy_bday = Song.new(happy_bday_lyrics)
 
-bulls_on_parade = Song.new(["They rally around tha family", 
-                    "With pockets full of shellls"])
 
-ring = Song.new (["Ring-a-ring-rosies", "A pocket full of posies",
-                    "A tissue, a tissue", "We all fall down"])
+bulls_lyrics = ["They rally around tha family", 
+    "With pockets full of shellls"]
+bulls_on_parade = Song.new(bulls_lyrics)
+
+ring_lyrics = ["Ring-a-ring-rosies", "A pocket full of posies",
+    "A tissue, a tissue", "We all fall down"]
+ring = Song.new (ring_lyrics)
 
 # object.method.argument
 # call sing_me_a_song on happy_bday
